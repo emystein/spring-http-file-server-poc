9c1244df581fcf9ceb6f2bcc047ac2230346a4be
Emiliano Menéndez <emystein@gmail.com>
2023-02-18 16:31:43 -0300

    LocalFileServerControllerTest

---
+++ src/test/kotlin/ar/com/flow/download/controller/LocalFileServerControllerTest.kt
@@ -0,0 +1,38 @@
+package ar.com.flow.download.controller
+
+import org.apache.commons.io.FileUtils
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.ActiveProfiles
+import java.io.File
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class LocalFileServerControllerTest {
+    @Value(value = "\${local.server.port}")
+    private var port = 0
+
+    @Value(value = "\${local.source.path}")
+    private lateinit var localServerDirectory: String
+
+    @Autowired
+    private lateinit var restTemplate: TestRestTemplate
+
+    @BeforeEach
+    fun setUp() {
+        FileUtils.copyFile(File("src/test/resources/hello_world.txt"), File("$localServerDirectory/hello_world.txt"))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun serveByteArray() {
+        val url = "http://localhost:$port/files/bytearray/hello_world.txt"
+        assertThat(restTemplate.getForObject(url, String::class.java)).contains("Hello, World")
+    }
+}
