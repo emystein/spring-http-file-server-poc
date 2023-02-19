750ce7fd069c4eb271022ea1c2b3dcbe03398e6d
Emiliano Menéndez <emystein@gmail.com>
2023-02-19 15:37:57 -0300

    Replace unirest-java with okhttp

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.konghq:unirest-java:3.13.0")
+	implementation("com.squareup.okhttp3:okhttp")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("com.squareup.okhttp3:mockwebserver")
--- src/main/kotlin/ar/com/flow/download/server/FileServerProxy.kt
+++ src/main/kotlin/ar/com/flow/download/server/FileServerProxy.kt
@@ -1,9 +1,10 @@
 package ar.com.flow.download.server
 
 import ar.com.flow.download.AttachmentContentDisposition
-import kong.unirest.Unirest
-import org.apache.commons.io.IOUtils
 import jakarta.servlet.http.HttpServletResponse
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.apache.commons.io.IOUtils
 
 class FileServerProxy(private val remoteSourceUrl: String) {
     fun read(fileName: String): RemoteFileToServe {
@@ -23,8 +24,11 @@ class RemoteFileToServe(remoteSourceUrl: String, private val fileName: String) {
     fun attachTo(response: HttpServletResponse) {
         AttachmentContentDisposition(fileName).setTo(response)
 
-        Unirest.get(filePath).asObject { rawResponse ->
-            IOUtils.copyLarge(rawResponse.content, response.outputStream)
+        val client = OkHttpClient()
+        val request = Request.Builder().url(filePath).build()
+
+        client.newCall(request).execute().use { remoteResponse ->
+            IOUtils.copyLarge(remoteResponse.body!!.byteStream(), response.outputStream)
         }
     }
 }
